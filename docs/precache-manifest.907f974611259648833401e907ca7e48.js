8879771462f9477640359ad4bc38d833202dc022
JustMonk <night.fire@mail.ru>
2019-11-12 20:35:10 +0500

    add demo

---
+++ docs/precache-manifest.907f974611259648833401e907ca7e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5740636f19c40a045b9119a610971a81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72dfd48f25f3c5e6d2f3",
+    "url": "/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "8444f2bacd8234b1e831",
+    "url": "/static/css/main.3dcc75d8.chunk.css"
+  },
+  {
+    "revision": "72dfd48f25f3c5e6d2f3",
+    "url": "/static/js/2.1a0ee992.chunk.js"
+  },
+  {
+    "revision": "8444f2bacd8234b1e831",
+    "url": "/static/js/main.06ad181e.chunk.js"
+  },
+  {
+    "revision": "fade95416195c6a111c7",
+    "url": "/static/js/runtime-main.1a880369.js"
+  }
+]);
